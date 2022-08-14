759815f39def292104dc0a9f22d5bfdf6bccd77b
Ekas Preet Singh <ekas@27pilots.com>
2022-08-14 21:00:21 +0200

    feat: Add global colors variables to craco config

--- frontend/craco.config.js
+++ frontend/craco.config.js
@@ -8,6 +8,15 @@ module.exports = {
         lessLoaderOptions: {
           lessOptions: {
             javascriptEnabled: true,
+            modifyVars: {
+              "@swatch_1": "#e0e8e7",
+              "@swatch_2": "#bccac1",
+              "@swatch_3": "#6aa7b3",
+              "@swatch_4": "#5f5f5f",
+              "@swatch_5": "#787b85",
+              "@swatch_6": "#2f3433",
+              "@swatch_7": "#f04c0f",
+            },
           },
         },
       },
