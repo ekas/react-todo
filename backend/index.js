43fc223e38392dac3a057808534c30991bfbbbde
Ekas <ekaspreet93.singh@gmail.com>
2022-08-17 20:18:55 +0200

    feat: Update backend API

--- backend/index.js
+++ backend/index.js
@@ -2,13 +2,34 @@ var express = require("express");
 var app = express();
 var bodyParser = require("body-parser");
 var logger = require("morgan");
+var uuid = require("uuid");
 
-var tasks = {
-  1: { id: "1", text: "Read description of programming challenge" },
-  2: { id: "2", text: "Implement awesome web app" },
-  3: { id: "3", text: "Polish project" },
-  9: { id: "9", text: "Send solution to LogMeIn" },
-};
+var tasks = [
+  {
+    id: uuid.v4(),
+    title: "Task 1",
+    status: "todo",
+    description: "Read description of programming challenge",
+  },
+  {
+    id: uuid.v4(),
+    title: "Task 2",
+    status: "todo",
+    description: "Implement awesome web app",
+  },
+  {
+    id: uuid.v4(),
+    title: "Task 3",
+    status: "completed",
+    description: "Polish project",
+  },
+  {
+    id: uuid.v4(),
+    title: "Task 4",
+    status: "inprogress",
+    description: "Send solution to LogMeIn",
+  },
+];
 
 app.use(logger("dev"));
 app.use(bodyParser.json());
@@ -29,7 +50,13 @@ app.get("/api/tasks", function (req, res) {
 
 app.post("/api/tasks", function (req, res) {
   console.log("POST a new Task", req.body);
-  if (!req.body || !req.body.id || !req.body.text) {
+  if (
+    !req.body ||
+    !req.body.id ||
+    !req.body.description ||
+    !req.body.title ||
+    !req.body.status
+  ) {
     res.status(400).send("Invalid task format");
     return;
   }
@@ -40,19 +67,28 @@ app.post("/api/tasks", function (req, res) {
 
   tasks[req.body.id] = {
     id: req.body.id,
-    text: req.body.text,
+    title: req.body.title,
+    status: req.body.status,
+    description: req.body.description,
   };
   res.status(204).send();
 });
 
 app.put("/api/tasks/:id", function (req, res) {
   console.log("PUT a new Task", req.params.id, req.body);
+
   if (!tasks[req.params.id]) {
     res.status(404).send();
     return;
   }
 
-  tasks[req.params.id].text = req.body.text;
+  const { id, title, status, description } = req.body;
+  tasks[req.params.id] = {
+    id: id,
+    title: title,
+    status: status,
+    description: description,
+  };
   res.status(204).send();
 });
 
